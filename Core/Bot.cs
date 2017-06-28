e9b7bb001dd9f7b3dae7ffedc4c078bf269a0285
PeterPE128 <peter@jazzter.nl>
2017-06-28 21:29:16 +0200

    Initial Changes
    Description

---
+++ Core/Bot.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+
+namespace BSBot
+{
+    class MyBot
+    {
+        DiscordClient discord;
+        CommandService commands;
+
+        public Core()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '*';
+                x.AllowMentionPrefix = true;
+                //x.HelpMode = HelpMode.Public;            
+            });
+
+            commands = discord.GetService<CommandService>();
+
+            Commands();
+
+            discord.ExecuteAndWait(async () =>
+            {
+                while (true)
+                {
+                    await discord.Connect("token", TokenType.Bot);
+                    Console.WriteLine("Bot connected correctly");
+                    discord.SetGame("%help for commands");
+                    break;
+                }
+            });
+        }
+
+
+
+
+
+
+
+
+            static void Main(string[] args)
+        {
+        }
+    }
+}
